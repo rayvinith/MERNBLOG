447028679b55b901f23fdcc04f0acfab98b12af7
rayvinith <rayaniket6404@gmail.com>
2025-01-26 17:23:24 +0530

    this is full stcak blog app

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="text-3xl bg-red-100 bg-amber-100">
+      <h1>hello</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
